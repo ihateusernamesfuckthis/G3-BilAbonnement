afbab21e43da518071e5c0c7ae20f3e1f2d847da
ihateusernamesfuckthis <pepe0003@kea.stud.dk>
2024-11-26 15:07:13 +0100

    Initial Commit

---
+++ src/main/java/com/example/g3bilabonnement/G3BilAbonnementApplication.java
@@ -0,0 +1,13 @@
+package com.example.g3bilabonnement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G3BilAbonnementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G3BilAbonnementApplication.class, args);
+    }
+
+}
